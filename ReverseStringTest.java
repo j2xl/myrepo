74f5a93fdf2230a1d0d390a99a0d06b764c11d3b
Jason Weill <jason@weill.org>
2014-08-09 18:53:12 -0700

    Adds first unit test

--- ReverseStringTest.java
+++ ReverseStringTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import homework.ReverseString;
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
  * Tests for {@Link ReverseString}.
@@ -14,4 +14,14 @@ import org.junit.runners.JUnit4;
  * @author jason@weill.org (Jason Weill)
  */
 public class ReverseStringTest {
+    @Test
+    public void testEmptyString() {
+        String empty = new String("");
+        String reversedEmpty = homework.ReverseString.reverseString(empty);
+        org.junit.Assert.assertEquals(
+            "Failed to reverse empty string", 
+            empty,
+            reversedEmpty
+        );
+    }
 }
